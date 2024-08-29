88b69b6ca0b0963ae775d66211bded5082ad6676
Samir Jabre <73381924>
2024-08-30 02:22:24 +0300

    Building the structure of the page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,3 +3,11 @@ import { useEffect, useState } from 'react'
 import { StyleSheet, Text, View, BackHandler , SafeAreaView, Image, TouchableOpacity} from 'react-native'
 import { useFonts } from 'expo-font';
 import { SplashScreen } from 'expo-router';
+
+const home = () => {
+  const [message, setMessage] = useState('')
+  useEffect(() => {
+    AsyncStorage.getItem('Message')
+    const backAction = () => {
+      return true;
+    };
