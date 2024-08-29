aec23719a296f34f997289c79949ec39188a2525
Samir Jabre <73381924>
2024-08-30 02:21:54 +0300

    Importing the needed hooks

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,1 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, BackHandler , SafeAreaView, Image, TouchableOpacity} from 'react-native'
+import { useFonts } from 'expo-font';
+import { SplashScreen } from 'expo-router';
