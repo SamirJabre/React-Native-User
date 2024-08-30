4f54aeb4478c28f70947473b69ab0bcff184ee69
Samir Jabre <73381924>
2024-08-30 20:17:27 +0300

    Created the result page

---
+++ app/(tabs)/result.jsx
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet , Text , View , TouchableOpacity, SafeAreaView , ScrollView, Image } from 'react-native';
+import {  useState } from 'react';
+import * as React from 'react';
+import Authback from '../../components/Authback';
+import FilterToggle from '../../components/FilterToggle';
+
