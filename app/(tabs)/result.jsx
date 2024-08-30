a81d0546559131f081b373300b7fcfd3436a01f8
Samir Jabre <73381924>
2024-08-30 20:18:00 +0300

    Fixing Bugs

--- app/(tabs)/result.jsx
+++ app/(tabs)/result.jsx
@@ -12,3 +12,22 @@ export default function result() {
   const [latest, setLatest] = useState(false);
   const [rating, setRating] = useState(false);
 
+
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <View style={styles.container}>
+        <View style={styles.backBtncontainer}>
+          <Authback/>
+        </View>
+        <Text style={styles.upcomingText}>Upcoming Buses</Text>
+
+        <View style={styles.filters}>
+        <FilterToggle text='Price' onpress={()=>setPrice(!price)} status={price}/>
+        <FilterToggle text='Time' onpress={()=>setTime(!time)} status={time}/>
+        <FilterToggle text='Latest' onpress={()=>setLatest(!latest)} status={latest}/>
+        <FilterToggle text='Rating' onpress={()=>setRating(!rating)} status={rating}/>
+        </View>
+        <ScrollView style={styles.buses}>
+          
+
+          
