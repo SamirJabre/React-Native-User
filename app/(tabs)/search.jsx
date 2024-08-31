3cb289595e49b801b4ebc3598d22d54bafa30822
Samir Jabre <73381924>
2024-08-31 20:56:30 +0300

    Started with the search page

---
+++ app/(tabs)/search.jsx
@@ -0,0 +1,6 @@
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import NavigationBar from '../../components/NavigationBar'
+import { StatusBar } from 'expo-status-bar'
+import Slider from '@react-native-community/slider';
+import { Picker } from '@react-native-picker/picker';
