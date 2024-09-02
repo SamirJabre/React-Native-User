fe71b68179b04547fdc13c38de8cac59bfbbf0d7
Samir Jabre <73381924>
2024-09-03 01:31:15 +0300

    Designing the Page

--- app/(tabs)/tripInfo.jsx
+++ app/(tabs)/tripInfo.jsx
@@ -1,10 +1,14 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import NavigationBar from '../../components/NavigationBar'
 
 const tripInfo = () => {
   return (
     <SafeAreaView style={styles.safearea}>
+    <View style={styles.container}>
 
+    </View>
+    <NavigationBar/>    
     </SafeAreaView>
   )
 }
@@ -18,5 +22,10 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     marginTop: 0,
     justifyContent: 'space-between',
+  },
+  container:{
+    width : '100%',
+    height: '80%',
+    backgroundColor:'blue',
   },
 })
