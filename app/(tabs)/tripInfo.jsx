a0b1ff4a11c47a4ff2bdcc192e6f6ed0df6c715d
Samir Jabre <73381924>
2024-09-03 03:01:34 +0300

    Building the Trip Info Page

--- app/(tabs)/tripInfo.jsx
+++ app/(tabs)/tripInfo.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import NavigationBar from '../../components/NavigationBar'
 
@@ -6,6 +6,17 @@ const tripInfo = () => {
   return (
     <SafeAreaView style={styles.safearea}>
     <View style={styles.container}>
+    
+    <View style={styles.upperBox}>
+        <View style={styles.upperPart}>
+            <TouchableOpacity style={styles.backBtn}>
+              <Image source={require('../../assets/icons/back_arrow.png')} style={{height:30,width:30}}/>
+            </TouchableOpacity>
+            <Text style={styles.busText}>Bus Booking</Text>
+        </View>
+    </View>
+
+    <View style={styles.mapContainer}></View>
 
     </View>
     <NavigationBar/>    
@@ -25,7 +36,35 @@ const styles = StyleSheet.create({
   },
   container:{
     width : '100%',
-    height: '80%',
+    height: '92%',
     backgroundColor:'blue',
+  },
+  upperBox:{
+    height:'15%',
+    width:'100%',
+    backgroundColor:'#0C3B2E',
+  },
+  upperPart:{
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'50%',
+      width:'100%',
+      backgroundColor:'yellow',
+  },
+  backBtn:{
+    height:'100%',
+    width:'20%',
+    backgroundColor:'green',
+    justifyContent:'center',
+    alignItems:'flex-end',
+  },
+  busText:{
+
+  },
+  mapContainer:{
+    width:'100%',
+    height:'40%',
+    backgroundColor:'red',
   },
 })
