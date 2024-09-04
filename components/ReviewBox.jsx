17a57e37423ac22598cea21f05b78047d860dabf
Samir Jabre <73381924>
2024-09-05 01:21:52 +0300

    Adding Styles

--- components/ReviewBox.jsx
+++ components/ReviewBox.jsx
@@ -21,4 +21,46 @@ const ReviewBox = ({userName,user_pp,comment,created_at}) => {
 }
 export default ReviewBox
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    reviewBox:{
+        height:'15%',
+        width:'100%',
+        flexDirection:'row',
+        borderBottomWidth:1,
+        borderBottomColor:'#5E5E5E',
+    },
+    left:{
+        height:'100%',
+        width:'75%',
+        justifyContent:'flex-start',
+        alignItems:'center',
+        flexDirection:'row',
+    },
+    content:{
+        height:'50%',
+        width:'100%',
+        justifyContent:'space-evenly',
+    },
+    userName:{
+        color:'#0C3B2E',
+        fontSize:16,
+        fontFamily:'Inter-SemiBold',
+    },
+    comment:{
+        fontSize:12,
+        fontFamily:'Inter-Regular',
+        color:'#5E5E5E',
+    },
+    right:{
+        height:'100%',
+        width:'25%',
+        justifyContent:'flex-end',
+        alignItems:'center',
+        padding:10,
+    },
+    date:{
+        color:'#5E5E5E',
+        fontFamily:'Inter-Regular',
+        fontSize:10,
+    },
+})
