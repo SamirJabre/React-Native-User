7a6ff3edffc1f7f8446093fe1df8efda32f3daf3
Samir Jabre <73381924>
2024-09-04 12:55:29 +0300

    Importing the important things

--- app/(tabs)/result.jsx
+++ app/(tabs)/result.jsx
@@ -1,5 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet , Text , View , TouchableOpacity, SafeAreaView , ScrollView, Image } from 'react-native';
+import {  useState ,useEffect } from 'react';
+import * as React from 'react';
+import Authback from '../../components/Authback';
+import FilterToggle from '../../components/FilterToggle';
+import { router, useLocalSearchParams } from 'expo-router';
+import NavigationBar from '../../components/NavigationBar';
+import BusBox from '../../components/BusBox';
+import axios from 'axios';
+import { BASE_URL } from '@env';
 
 const result = () => {
   return (
