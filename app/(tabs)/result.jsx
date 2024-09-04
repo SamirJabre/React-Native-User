266f8f8e85a043d5e55943fef44e363a519f6a1d
Samir Jabre <73381924>
2024-09-04 12:56:29 +0300

    Fixing Bugs

--- app/(tabs)/result.jsx
+++ app/(tabs)/result.jsx
@@ -27,7 +27,45 @@ export default function result() {
     })
     .then(res=>setTrips(res.data))
   },[])
+  
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <View style={styles.container}>
+        <View style={styles.backBtncontainer}>
+          <Authback onpress={()=> router.push('/search')}/>
+        </View>
+        <Text style={styles.upcomingText}>Search Result</Text>
 
+        <View style={styles.filters}>
+        <FilterToggle text='Price' onpress={()=>setPrice(!price)} status={price}/>
+        <FilterToggle text='Time' onpress={()=>setTime(!time)} status={time}/>
+        <FilterToggle text='Latest' onpress={()=>setLatest(!latest)} status={latest}/>
+        <FilterToggle text='Rating' onpress={()=>setRating(!rating)} status={rating}/>
+        </View>
+        <ScrollView style={styles.buses} >
+        {trips.map((trip)=>{
+          return <BusBox 
+          key={trip.id} 
+          from={trip.from} 
+          to={trip.to} 
+          price={trip.price} 
+          seats={trip.passenger_load}
+          driver={trip.name}
+          date={trip.date}
+          departure={trip.departure_time}
+          rating={trip.rating}
+          onpress={()=>router.push(`/tripInfo?tripId=${trip.id}`)}
+          />
+        })}
+        </ScrollView>
+
+      </View>
+    
+    <StatusBar style='dark'/>
+    <NavigationBar/>
+    </SafeAreaView>
+  );
+}
 export default result
 
 const styles = StyleSheet.create({})
