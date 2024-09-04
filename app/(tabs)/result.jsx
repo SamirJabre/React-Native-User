2aee9552c6770305009c70a4d7a366de7f6b7e90
Samir Jabre <73381924>
2024-09-04 05:26:28 +0300

    Finished the Search Functionality and make it worked

--- app/(tabs)/result.jsx
+++ app/(tabs)/result.jsx
@@ -6,17 +6,26 @@ import Authback from '../../components/Authback';
 import FilterToggle from '../../components/FilterToggle';
 import { router, useLocalSearchParams } from 'expo-router';
 import NavigationBar from '../../components/NavigationBar';
+import BusBox from '../../components/BusBox';
+import axios from 'axios';
+import { BASE_URL } from '@env';
 
 export default function result() {
-  const {  data  } = useLocalSearchParams();
+  const {  from , to , ticket , date  } = useLocalSearchParams();
   const [price, setPrice] = useState(false);
   const [time, setTime] = useState(false);
   const [latest, setLatest] = useState(false);
   const [rating, setRating] = useState(false);
+  const [trips, setTrips] = useState([]);
 
   useEffect(()=>{
-    console.log(data);
-    
+    axios.post(`${BASE_URL}/search` ,{
+      from: from,
+      to: to,
+      price: ticket,
+      date: date
+    })
+    .then(res=>setTrips(res.data))
   },[])
 
   return (
@@ -33,31 +42,21 @@ export default function result() {
         <FilterToggle text='Latest' onpress={()=>setLatest(!latest)} status={latest}/>
         <FilterToggle text='Rating' onpress={()=>setRating(!rating)} status={rating}/>
         </View>
-        <ScrollView style={styles.buses}>
-          
-
-          <TouchableOpacity style={styles.bus}>
-
-            <View style={styles.left}>
-              <Text style={styles.busInfo}>From: Tripoli</Text>
-              <Text style={styles.busInfo}>To: Beirut</Text>
-              <Text style={styles.busInfo}>Driver: Samir Jabre</Text>
-            </View>
-
-            <View style={styles.right}>
-              <Text style={styles.busInfo2}>Price: 10$</Text>
-              <Text style={styles.busInfo2}>Seats: 50</Text>
-              <View style={styles.images}>
-                <Image source={require('../../assets/icons/star.png')} style={styles.image}/>
-                <Image source={require('../../assets/icons/star.png')} style={styles.image}/>
-                <Image source={require('../../assets/icons/star.png')} style={styles.image}/>
-                <Image source={require('../../assets/icons/star.png')} style={styles.image}/>
-                <Image source={require('../../assets/icons/star.png')} style={styles.image}/>
-              </View>
-            </View>
-
-          </TouchableOpacity>
-        
+        <ScrollView style={styles.buses} >
+        {trips.map((trip)=>{
+          return <BusBox 
+          key={trip.id} 
+          from={trip.from} 
+          to={trip.to} 
+          price={trip.price} 
+          seats={trip.passenger_load}
+          driver={trip.name}
+          date={trip.date}
+          departure={trip.departure_time}
+          rating={trip.rating}
+          onpress={()=>router.push(`/tripInfo?tripId=${trip.id}`)}
+          />
+        })}
         </ScrollView>
 
       </View>
