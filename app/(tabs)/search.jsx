58133ab6b97e4c33e32dcb04bb8cffc2db2fb949
Samir Jabre <73381924>
2024-09-04 12:53:47 +0300

    Fixing Bugs

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -44,8 +44,6 @@ const search = () => {
     </View>
     <Image source={require('../../assets/bus-background.png')} style={styles.image}/>
     </View>
-    Image source={require('../../assets/bus-background.png')} style={styles.image}/>
-    </View>
 
     <View style={styles.inputContainer}>
     <View style={styles.left}>
@@ -60,15 +58,6 @@ const search = () => {
     <View style={styles.low}>
     <Text style={{fontSize:12, color:'#B8B8B8', margin:5, fontFamily:'Inter-Regular'}}>To</Text>
     <TextInput style={styles.input} value={to} onChangeText={setTo}></TextInput>
-    </View>
-
-    </View>
-
-
-    <View style={styles.right}>
-      <TouchableOpacity style={{height:35,width:35}} onPress={switchInputs}>
-      <Image source={require('../../assets/icons/switch.png')} style={{height:"100%",width:"100%"}}/>
-      </TouchableOpacity>
     </View>
 
     </View>
