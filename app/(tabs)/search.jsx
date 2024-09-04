0ca63d426f8b0b73e4d068b0152cb8bc839e7825
Samir Jabre <73381924>
2024-09-04 12:52:38 +0300

    Setting the components

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -32,3 +32,15 @@ const search = () => {
     router.push(`/result?from=${from}&to=${to}&ticket=${price}&date=${date}`)
   }
   
+
+  return (
+    <SafeAreaView style={styles.safearea}>
+    <View style={styles.container}>
+    <View style={styles.upperbox}>
+    <Text style={{marginTop:10,fontSize:20,color:'white',fontFamily:'Inter-SemiBold'}}>Bus Booking</Text>
+    <View style={styles.greeting}>
+    <Text style={styles.hello}>Hello,</Text>
+    <Text style={styles.title}>Search +100 Bus For Your Journey</Text>
+    </View>
+    <Image source={require('../../assets/bus-background.png')} style={styles.image}/>
+    </View>
