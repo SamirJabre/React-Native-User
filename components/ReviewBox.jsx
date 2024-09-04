e3d8a74f4a6b511717b248f8a6dae259e249f328
Samir Jabre <73381924>
2024-09-05 01:21:28 +0300

    feat: Add user profile picture and date to ReviewBox component

--- components/ReviewBox.jsx
+++ components/ReviewBox.jsx
@@ -1,10 +1,21 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const ReviewBox = () => {
+const ReviewBox = ({userName,user_pp,comment,created_at}) => {
+    const dateOnly = created_at.split(' ')[0];
+    const pp= {uri:user_pp}
   return (
-    <View>
-      <Text>ReviewBox</Text>
+    <View style={styles.reviewBox}>
+    <View style={styles.left}>
+        <Image source={pp} style={{height:60,width:60,margin:5 , borderRadius:100}}/>
+        <View style={styles.content}>
+        <Text style={styles.userName}>{userName}</Text>
+        <Text style={styles.comment}>{comment}</Text>
+        </View>
+    </View>
+    <View style={styles.right}>
+        <Text style={styles.date}>{dateOnly}</Text>
+    </View>
     </View>
   )
 }
