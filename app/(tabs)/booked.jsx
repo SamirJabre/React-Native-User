574c6d9b8619f5a955558b9922318fe5133d87f4
Samir Jabre <samirjabre8@gmail.com>
2024-09-05 09:22:13 +0300

    Building the structure of the page

---
+++ app/(tabs)/booked.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const booked = () => {
+  return (
+    <View>
+      <Text>booked</Text>
+    </View>
+  )
+}
+
+export default booked
+
+const styles = StyleSheet.create({})
