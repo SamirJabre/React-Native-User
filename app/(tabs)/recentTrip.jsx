ed76975109d0f395b749b5ac766d44a8bfc0deb0
Samir Jabre <73381924>
2024-09-09 22:54:34 +0300

    Building the Structure of the Page

---
+++ app/(tabs)/recentTrip.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const recentTrip = () => {
+  return (
+    <View>
+      <Text>recentTrip</Text>
+    </View>
+  )
+}
+
+export default recentTrip
+
+const styles = StyleSheet.create({})
