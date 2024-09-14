f5dae86821561fa2849aca56f7b393e9ec02a94f
Samir Jabre <73381924>
2024-09-14 20:19:34 +0300

    Adding all the components

--- components/ChatBot.jsx
+++ components/ChatBot.jsx
@@ -24,9 +24,48 @@ const ChatBot = () => {
 
 
   return (
-    <View>
-      <Text>ChatBot</Text>
-    </View>
+    <>
+        <TouchableOpacity onPress={toggleChat} style={styles.chatButtonContainer}>
+      <Image style={{height:'70%', width:'70%'}} source={require('../assets/icons/bot.png')}/>
+      </TouchableOpacity>
+      <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isChatOpen}
+      onRequestClose={toggleChat}
+    >
+      <View style={styles.chatPopup}>
+
+        <View style={styles.chatHeader}>
+        <Text style={styles.headerText}>ChatBot</Text>
+        <TouchableOpacity onPress={toggleChat} style={styles.closeButton}>
+          <Image style={styles.closeButtonIcon} source={require('../assets/icons/close.png')}/>
+        </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={messages}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <View style={item.sender === 'bot' ? styles.botMessage : styles.userMessage}>
+              <Text>{item.text}</Text>
+            </View>
+          )}
+        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={input}
+            onChangeText={setInput}
+            placeholder="Type your message..."
+          />
+          <TouchableOpacity onPress={sendMessage} style={styles.sendButton}>
+            <Text style={styles.sendButtonText}>Send</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+    </>
   )
 }
 
